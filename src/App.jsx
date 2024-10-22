f248fc1bb8d8a93cecca2103e0823130f1600ec2
Sofia Bern <128867240+sofiabern@users.noreply.github.com>
2024-10-23 03:53:10 +0800

    components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Player from "./components/Player/Player";
+import GameBoard from "./components/GameBoard/GameBoard";
 function App() {
   return (
     <main>
       <div id="game-container">
         <ol id="players">
-          <Player name ="Player 1" symbol="X"/>
-          <Player  name ="Player 2" symbol="O"/>
+          <Player initialName ="Player 1" symbol="X"/>
+          <Player  initialName ="Player 2" symbol="O"/>
         </ol>
+        <GameBoard/>
       </div>
     </main>
   );
